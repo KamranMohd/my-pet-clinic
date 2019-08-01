81d242f13ff41d3aa4447ac21f9bf55ce70435f9
Mohd Kamran <kamran@Mohds-MacBook-Air.local>
2019-08-01 20:13:03 +0530

    POJO for pet clinic. Closes #2

---
+++ src/main/java/com/learning/spring/mypetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package com.learning.spring.mypetclinic.model;
+
+public class PetType {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
