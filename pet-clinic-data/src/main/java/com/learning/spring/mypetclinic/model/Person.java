79ac95d12b79a05c0df77c1fdad8954b878ea760
Mohd Kamran <kamran@Mohds-MacBook-Air.local>
2019-08-23 23:44:06 +0530

    implementing owner spring data jpa service. closes #39

--- pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/Person.java
@@ -12,7 +12,7 @@ public class Person extends BaseEntity{
 	@Column(name="first_name")
 	private String firstName;
 	
-	@Column(name="la st_name")
+	@Column(name="last_name")
 	private String lastName;
 	
 	public String getFirstName() {
--- pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/repositories/OwnerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.learning.spring.mypetclinic.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+	Owner findByLastName(String lastName);
 }
