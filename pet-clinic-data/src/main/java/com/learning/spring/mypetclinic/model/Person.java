81ce5f2c3074587fe9bcc84b7f65f4f536cb2522
Mohd Kamran <kamran@Mohds-MacBook-Air.local>
2019-08-23 22:49:37 +0530

    added owner JPA entity. closes #34

--- pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/learning/spring/mypetclinic/model/Person.java
@@ -1,7 +1,18 @@
 package com.learning.spring.mypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+//	@mappedSuperClass means that Person entity will never be stored in the database
+//	Only its sub classes will be added in the database
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+	
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="la st_name")
 	private String lastName;
 	
 	public String getFirstName() {
