64e0f04f7ce75f33dfb42f876087b45cabf2e11a
Mohd Kamran <kamran@Mohds-MacBook-Air.local>
2019-09-12 19:57:36 +0530

    display owner details. closes #52

--- pet-clinic-web/src/main/java/com/learning/spring/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/learning/spring/mypetclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package com.learning.spring.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.learning.spring.mypetclinic.services.OwnerService;
 
@@ -25,6 +28,13 @@ public class OwnerController {
 	@RequestMapping({"/owners/find","/oups"})
 	public String oupsHandler(){
 		return "notimplemented";
+	}
+	
+	@GetMapping("/owners/{ownerId}")
+	public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+		ModelAndView mav = new ModelAndView("owners/ownerDetails");
+		mav.addObject(ownerService.findById(ownerId));
+		return mav;
 	}
 }
 
